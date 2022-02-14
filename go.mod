8075ab28efa70ab4efd48d7336c44910d945f8e8
Don Mennerich <don.mennerich@nyu.edu>
2022-02-14 10:04:28 -0500

    updating notes.txt, version of go-bagit

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/nyudlts/Archivematica-Package-Prep
 
 go 1.17
 
-require github.com/nyudlts/go-bagit v0.1.1-alpha.0.20220209192346-2c25d62f6231
+require github.com/nyudlts/go-bagit v0.1.1-alpha.0.20220214141947-20f7d3004abe
--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	go_bagit "github.com/nyudlts/go-bagit"
+	"os"
 )
 
+var bag string
+
+func init() {
+	flag.StringVar(&bag, "bag", "", "location of bag")
+}
 func main() {
-	testBag := "./test-bag/"
-	fmt.Println(testBag)
-	if err := go_bagit.ValidateBag(testBag, false, false); err != nil {
+	flag.Parse()
+	go_bagit.Logger().SetOutput(os.Stderr)
+
+	if err := go_bagit.ValidateBag(bag, false, false); err != nil {
 		fmt.Println(err)
 	}
 }
